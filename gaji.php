0adaaf60f555b75bfa0a879a7fb1d69d379a3ee0
akrama3 <akramaakunsma@gmail.com>
2025-05-23 18:54:12 +0700

    shie

--- gaji.php
+++ gaji.php
@@ -3,17 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>    
+    <title>Data Gaji Karyawan</title>    
     <link rel="stylesheet" href="gaji.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
- 
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+    <?php include "koneksi.php";
+     $query = "SELECT k.nama, k.id_karyawan, k.jabatan, g.gaji_pokok, g.potongan, g.total_gaji, g.bulan 
+                  FROM gaji g
+                  JOIN karyawan k ON g.id_karyawan = k.id_karyawan
+                  ORDER BY g.bulan DESC";
+        
+        $result = $konek->query($query);
+    ?>
     <!-- Sidebar -->
     <div class="sidebar">
         <ul class="isi-list">
@@ -21,13 +26,8 @@
                 <li>
                     <i class="fas fa-user"></i>
                     <span>
-                        <h4>
-                            <?php echo $nama?>
-                        </h4>
-                        <p>
-                            <?php echo $jabatan?>
-                        </p>
-                        testdsiagdlcwygdq;as
+                        <h4><?php echo isset($nama) ? $nama : 'Admin'; ?></h4>
+                        <p><?php echo isset($jabatan) ? $jabatan : 'Administrator'; ?></p>
                     </span>
                 </li>
             </div>
@@ -41,8 +41,7 @@
             </li>
             <li>
                 <i class="fa-solid fa-file-invoice"></i>
-                <span><a href="slipGaji.php">Slip Gaji</a>
-                </span>
+                <span><a href="slipGaji.php">Slip Gaji</a></span>
             </li>
             <li class="log-out-tombol">
                 <i class="fa-solid fa-right-from-bracket"></i>
@@ -51,37 +50,52 @@
         </ul>
     </div>
 
-
-
-
-        <table border='1'>
-            <tr>
-                <th>Nama</th>
-                <th>Id karyawan</th>
-                <th>Jabatan</th>
-                <th>Gaji</th>
-                <th>Lainnya</th>
-            </tr>
-            <tr>
+    <div class="container">
+   
+        
+        <h2>Data Gaji Karyawan</h2>
+        
+        <table border='1' class="salary-table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama Karyawan</th>
+                    <th>ID Karyawan</th>
+                    <th>Jabatan</th>
+                    <th>Bulan</th>
+                    <th>Gaji Pokok</th>
+                    <th>Potongan</th>
+                    <th>Total Gaji</th>
+                </tr>
+            </thead>
+            <tbody>
                 <?php
-            while ($row = $hasildata->fetch_array()) {
-            ?>
-                <td>
-                    <?=$row['nama']?>
-                </td>
-                <td>
-                    <?=$row['id_karyawan']?>
-                </td>
-                <td>
-                    <?=$row['jabatan']?>
-                </td>
-                <td>
-                    <?=$row['gaji']?>
-                </td>
-                <td>
-                    <?=$row['blablabla']?>
-                </td>
-            </tr>
-            <?php } ?>
+                if ($result->num_rows > 0) {
+                    $no = 1;
+                    while ($row = $result->fetch_assoc()) {
+                        $total_gaji = $row['gaji_pokok'] - $row['potongan'];
+                        echo "<tr>
+                                <td>{$no}</td>
+                                <td>{$row['nama']}</td>
+                                <td>{$row['id_karyawan']}</td>
+                                <td>{$row['jabatan']}</td>
+                                <td>{$row['bulan']}</td>
+                                <td>Rp " . number_format($row['gaji_pokok'], 0, ',', '.') . "</td>
+                                <td>Rp " . number_format($row['potongan'], 0, ',', '.') . "</td>
+                                <td>Rp " . number_format($total_gaji, 0, ',', '.') . "</td>
+                              </tr>";
+                        $no++;
+                    }
+                } else {
+                    echo "<tr><td colspan='8'>Tidak ada data gaji</td></tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+        
+        <?php
+        $konek->close();
+        ?>
+    </div>
 </body>
 </html>
